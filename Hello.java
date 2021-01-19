0bd8f5c62bd75d4d09115e1c93e8cdaae231251d
RZP <raihanprayogi48@gmail.com>
2021-01-19 21:38:45 +0700

    Third Commit

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ class Hello{
 	public static void main(String[] args){
 		System.out.println("imam3890: Aplikasi Java 2020");
 		System.out.println("USER 2 RAIHAN");
+		System.out.println("Commit User 2");
 	}
 }
